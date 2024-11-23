fd5bd4778f93bb916e88a0c3920ab41af84bffca
Adam Wolf <adam@travefy.com>
2024-11-23 12:44:16 -0600

    idk

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
 export XDG_CONFIG_HOME="$HOME/.config"
-mkdir -p "$XDG_CONFIG_HOME"
-mkdir -p "$XDG_CONFIG_HOME/nixpkgs"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" && git submodule update --init
 
-ln -sf "$SCRIPT_DIR/config.nix" "$XDG_CONFIG_HOME/nixpkgs"
+sudo apt install -y stow
+stow -d "$SCRIPT_DIR" -t "$HOME"
 
 nix-env -iA nixpkgs.devcontainer
-
-stow -d "$SCRIPT_DIR" -t "$HOME"
