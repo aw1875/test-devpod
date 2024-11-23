e1406f77948dcbb4f3c04793412ffb59c72e1fd5
Adam Wolf <adam@travefy.com>
2024-11-23 12:46:23 -0600

    again

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" && git submodule update --init
 
 sudo apt install -y stow
-stow -d "$SCRIPT_DIR" -t "$HOME"
+cd "$SCRIPT_DIR" && stow . && cd -
 
 nix-env -iA nixpkgs.devcontainer
